a2fa98c1544d1d5552958d3a70e32732f0ab43df
TheFrenchCoder <raphaelouvigny@gmail.com>
2019-03-02 12:00:19 +0400

    Create 1.sql
    Essaie d'une requête permettant de selectionner les competiteurs pour les resultats en fonction de leur sexe, categorie (sous forme de n° allant de 0 à 7) et en fonction de leur temps final (ascendant, le plus petit en haut)

---
+++ 1.sql
@@ -0,0 +1,16 @@
+SELECT race1.start_time, 
+               race1.finish_time, 
+               race1.penalty, 
+               race1.result_time, 
+               competitors.number, 
+               competitors.name, 
+               competitors.firstname, 
+               competitors.club_abrev 
+          FROM race1 
+               INNER JOIN competitors 
+                         ON race1.number = competitors.number 
+          WHERE  competitors.ishere = 1 
+          AND competitors.isfinish = 1 
+          ORDER BY competitors.sex, 
+                    competitors.categorie_number, 
+                    race1.result_time ASC
