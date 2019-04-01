afa1e8a2807cd3b2ba0ab894e8a6fa6f25a30395
TheFrenchCoder <raphaelouvigny@gmail.com>
2019-04-01 22:15:59 +0400

    Refactoring non necessary code
    Add of:
    - sql2.txt => Request for obtain competitors has finish
    - forum => a request for help on a forum
    
    Refactoring of:
    - php/Tstart.php => adding a ';' at the end of the line
    - 1.sql => test for a better request of the finish page

--- 1.sql
+++ 1.sql
@@ -1,4 +1,5 @@
-SELECT race1.start_time, 
+SELECT 
+               race1.start_time, 
                race1.finish_time, 
                race1.penalty, 
                race1.result_time, 
@@ -6,11 +7,25 @@ SELECT race1.start_time,
                competitors.name, 
                competitors.firstname, 
                competitors.club_abrev 
-          FROM race1 
+	FROM race1 
                INNER JOIN competitors 
                          ON race1.number = competitors.number 
           WHERE  competitors.ishere = 1 
-          AND competitors.isfinish = 1 
+          AND competitors.isfinish = 1
+          AND competitors.categorie_number = $categorie_number
           ORDER BY competitors.sex, 
                     competitors.categorie_number, 
                     race1.result_time ASC
+
+
+
+/*
+SELECT *
+FROM table
+WHERE condition
+GROUP BY expression
+HAVING condition
+{ UNION | INTERSECT | EXCEPT }
+ORDER BY expression
+LIMIT count
+OFFSET start
--- php/Tstart.php
+++ php/Tstart.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_GET[start]) && isset($_GET[number]) && isset($_GET(missing))) {
-    $infos["miss"] = "Dossard n°" . $_GET[number] . " est absent"
+    $infos["miss"] = "Dossard n°" . $_GET[number] . " est absent";
 }else{
     $corrections["send"] = "Le départ n°" . $_GET[number] . " a été donné";
 }
