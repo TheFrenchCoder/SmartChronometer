39fd4476eb35cb28791bc4034cac2bf747e3805f
TheFrenchCoder <raphaelouvigny@gmail.com>
2019-04-14 20:14:20 +0400

    fix(result): By categories
    fixes the bug that when it occurred when the results were displayed all competitors obtained the start time, finish time, penalties and final time of the 1st competitor

--- php/results.php
+++ php/results.php
@@ -25,162 +25,173 @@ echo include_once "../include/bddConnectByRoot.php";
 
 <h1>Résultats:</h1>
 
-<?php //queries SQL
-    $qHasFinish = $bdd->query(
-    'SELECT * FROM competitors 
-    WHERE IsOnStart = 0 
-    AND IsOnRun = 0 
-    AND IsFinish = 1 
-    AND IsHere = 1');
-    $countFinish = $qHasFinish ->rowCount();
+<?php
+
+$arrCategories = array(
+	1 => "Poussin",
+	2 => "Benjamin",
+	3 => "Minime",
+	4 => "Cadet",
+	5 => "Junior",
+	6 => "Senior",
+     7 => "Vétérant");
+     
+     foreach ($arrCategories as $key => $value){
+
+		echo "
+		<div class=\"header\">
+
+		</div>
+
+		<table>
+			</TR>
+				<TH>Dossard</TH>
+				<TH>Nom Prénom</TH>
+				<TH>Club</TH>
+				<TH>Temps initial</TH>
+				<TH>Temps d'arrivée</TH>
+				<TH>Pénalitées</TH>
+				<TH>Temps final</TH>
+				<TH>Place</TH>
+			</TR>
+		";
+		
+		echo "$value<br/>";
+
+		$sqlHasFinish = 
+		('SELECT 
+			race1.start_time, 
+			race1.finish_time, 
+			race1.penalty, 
+			race1.result_time, 
+			competitors.number, 
+			competitors.name, 
+			competitors.firstname, 
+			competitors.club_abrev 
+		FROM 
+			race1 
+		INNER JOIN competitors 
+			ON race1.number = competitors.number 
+		WHERE 
+			competitors.ishere = 1 
+			AND competitors.isfinish = 1 
+			AND competitors.categorie_number = ? 
+		ORDER BY 
+			competitors.sex, 
+			competitors.categorie_number, 
+			race1.result_time ASC
+		');
+		$qHasFinish = $bdd->prepare($sqlHasFinish);
+		$qHasFinish->execute(array($key));
+
+		$DatasFinish = $qHasFinish->fetchAll();
+
+		//* RANK SET:
+		$rank = 0;
+		
+		foreach($DatasFinish as $DataFinish) {
+
+			// * Setting variables
+			$start_time = $DataFinish['start_time'];
+			$finish_time = $DataFinish['finish_time'];
+			$penalty = $DataFinish['penalty'];
+			$result_time = $DataFinish['result_time'];
+			$number = $DataFinish['number'];
+
+			// * Affectation des variables
+			// Stocke $data[firstname] dans une variable temporaire
+			$TfirstnameHasFinish = $DataFinish['firstname'];
+			// Stocke toutes la lettres sauf la 1ere dans une variable temporaire
+			$TrestHasFinish = substr($TfirstnameHasFinish, 1);
+			//LowerCase de $Trest en $rest
+			$restHasFinish = strtolower($TrestHasFinish);
+			//UpCase de $Tfirstname en $firstname
+			$firstname = strtoupper($TfirstnameHasFinish);
+			//UpCase de $data[name] en $name
+			$name = $DataFinish['name'];
+			// Stocke $data[club_abrev] dans une variable temporaire
+			$Tclub_abrevHasFinish = $DataFinish['club_abrev'];
+			//UpCase de $Tclub_abrev en $club_abrev
+			$club_abrev = strtoupper($Tclub_abrevHasFinish);
+
+			//* RANK :
+			$rank += 1;
+			
+			//TODO: ADD ALL OF THE CODE HERE :] ?>
+
+			
+			<div class="Dossard_Result">
+				</TR> <form method=\"GET\" action=\"\">
+				<?php echo "<TD> <p>$number</p> </TD>"?>
+			</div>
+		
+			<div class="Nom_Prenom_Result">
+				<?php
+				//Affiche le dossard du competiteur
+				echo "<TD><p>$name $firstname[0]$restHasFinish</p></TD>";
+				?>
+			</div>
+
+			<div class="Club_Result">
+				<?php
+				//Affiche le club du compétiteur
+				echo "<TD><p>$club_abrev</p></TD>";
+				?>
+			</div>
+
+			<div class="initialTime_Result">
+				<?php
+				//Affiche le temps de départ du compétiteur
+				echo "<TD> ". date('H:i:s.U', $start_time). " </TD>";
+			?>
+			</div>
+
+			<div class="finishTime_Result">
+				<?php
+				//Afficje le temps d'arrivée du compétiteur
+				echo "<TD> " .date('H:i:s.U', $finish_time). " </TD>";
+				?>
+			</div>
+			
+			<div class="Penalty_Result">
+				<?php
+				//SAffiche les pénalitées du compétiteur
+				echo "<TD> <p>$penalty</p> </TD>";
+				?>
+			</div>
+
+			<div class="FinalTime_Result">
+				<?php
+				echo "<TD> " .date("H:i:s.U", $result_time). " </TD>";
+				?>
+			</div>
+
+			<div class="Rank_Result">
+				<?php
+				if ($rank == 0) {
+					echo "<TD> undefined </TD>";
+					//! ERROR
+				} elseif ($rank == 1) {
+					echo "<TD> " .$rank. "er </TD>";
+				}elseif ($rank >= 2) {
+					echo "<TD> " .$rank. "ième </TD>";
+				}
+				
+				?>
+			</div>
+
+			<?php
+			echo "</TR>";  
+
+			echo '<br />';
+		}
+	   // * Séparation entre les tableaux des catégories et leur header contenant le nom de la catégorie 
+	   echo "<br/><br/>";
+    }
+    unset($value);
+    unset($key); 
 
 ?>
-<div class="table_HasFinish"> 
-
-    <table>
-
-    <div class="TR_HasFinish">
-        <?php echo "</TR>";
-        echo "<TH>Dossard</TH>"; 
-        echo "<TH>Nom Prénom</TH>"; 
-        echo "<TH>Club</TH>";
-        echo "<TH>Temps initial";
-        echo "<TH>Temps d'arrivée";
-        echo "<TH>Pénalitées</TH>"; 
-        echo "<TH>Temps final</TH>"; 
-        echo "</TR>";
-        ?>
-    </div>
-
-    <?php
-        foreach ($qHasFinish as $dataHasFinish) {
-
-        //SetUp les vriables
-        $sqlGettingFinishData = 
-          ('SELECT race1.start_time, 
-          race1.finish_time, 
-          race1.penalty, 
-          race1.result_time, 
-          competitors.number, 
-          competitors.name, 
-          competitors.firstname, 
-          competitors.club_abrev 
-     FROM race1 
-          INNER JOIN competitors 
-                    ON race1.number = competitors.number 
-     WHERE  competitors.ishere = 1 
-     AND competitors.isfinish = 1
-     ORDER BY competitors.sex, 
-               competitors.categorie_number,
-               race1.result_time ASC    ');
-
-
-        $qFinishData = $bdd->query($sqlGettingFinishData);
-
-        $FinishData = $qFinishData->fetch();
-        $startTime = $FinishData['start_time'];
-        $finishTime = $FinishData['finish_time'];
-        $penalty = $FinishData['penalty'];
-        $resultTime = $FinishData['result_time'];
-
-    
-    ?>
-    <div class="initData_HasFinish">
-          <?php
-          //Extraction et affectation des variables
-          // Stocke $data[firstname] dans une variable temporaire
-          $Tfirstname = $dataHasFinish['firstname']; 
-          // Stocke toutes la lettres sauf la 1ere dans une variable temporaire
-          $Trest = substr($Tfirstname, 1); 
-          //LowerCase de $Trest en $rest
-          $rest = strtolower($Trest); 
-          //UpCase de $Tfirstname en $firstname
-          $firstname = strtoupper($Tfirstname); 
-          //UpCase de $data[name] en $name
-          $name = strtoupper($dataHasFinish['name']); 
-          // Stocke $data[club_abrev] dans une variable temporaire
-          $Tclub_abrev = $dataHasFinish['club_abrev']; 
-          //UpCase de $Tclub_abrev en $club_abrev
-          $club_abrev = strtoupper($Tclub_abrev);
-          ?>
-    </div>
-
-     <div class="Dossard_HasFinish">
-          <?php  
-          //Dossard = $data[number]
-          echo "</TR> <form method=\"GET\" action=\"\">";
-          echo "
-               <TD>
-               <p>$dataHasFinish[number]</p>
-               </TD>  
-               ";
-          ?>
-     </div>
-        
-     <div class="Nom_Prenom_HasFinish">
-          <?php
-          //"Nom Prénom"=  $name $firstname[0]$rest
-          echo "<TD><p>$name $firstname[0]$rest</p></TD>";
-          ?>
-     </div>
-
-     <div class="Club_HasFinish">
-          <?php
-          //Club = $club_abrev
-          echo "<TD><p>$club_abrev</p></TD>";
-          ?>
-     </div>
-
-     <div class="StartTime_HasFinish">
-          <?php
-          //Checkbox pour savoir si le concurrent est ABS ou pas.
-          echo 
-          "
-          <TD> "
-        . date('H:i:s.U', $startTime) .
-          "</TD>
-          ";
-        ?>
-     </div>
-
-     <div class="StartTime_HasFinish">
-          <?php
-          //Checkbox pour savoir si le concurrent est ABS ou pas.
-          echo 
-          "
-          <TD> "
-         .date('H:i:s.U', $finishTime).
-          "</TD>
-          ";
-        ?>
-     </div>
-        
-     <div class="Penalty_HasFinish">
-          <?php
-          //Start = <input type="Submit">
-          echo "
-               <TD>
-               <p>$penalty</p>
-               </TD>
-               ";
-          ?>
-     </div>
-
-     <div class="FinalTime_HasFinish">
-     <?php
-     echo "
-          <TD>"
-          .date("H:i:s.U", $resultTime).
-          "</TD>
-          ";
-     ?>
-     </div>
-
-    <?php
-    echo "</TR>";  
-    } ?>
-    <table/>
-</div>
 
 <p class="tips">.</p>
 
@@ -204,4 +215,5 @@ echo "Session: connecté en tant que [" . $role . "]" . $_SESSION["username"];
 echo "<br/>";
 echo "<pre> " . var_dump($_SESSION) . " <pre/>";
 $bdd = null;
+
 ?>
