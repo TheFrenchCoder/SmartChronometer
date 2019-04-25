cafd4b99af6ce24cff6dafd63ba5113a0f4cb847
TheFrenchCoder <raphaelouvigny@gmail.com>
2019-04-25 21:15:30 +0400

    Fix(index.php): warning
    Warning due to the absence of the variable $infos in index.php, for fix it, I choose to remove the include(%/debug.php).

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ session_start();
 
 <?php
 //DEBUG
-include_once $_SERVER['DOCUMENT_ROOT']."/include/debug.php";
+//?include_once $_SERVER['DOCUMENT_ROOT']."/include/debug.php";
 
 //FOOTER
 include_once $_SERVER['DOCUMENT_ROOT']."/include/part/footer.php";
