d2d4d97395a7c1a4eeb8affb6d9d5df57deab67e
TheFrenchCoder <raphaelouvigny@gmail.com>
2019-07-03 15:21:49 +0400

    Refactor(judging.php): Use function and include ressources

--- include/judging.php
+++ include/judging.php
@@ -1,5 +1,10 @@
 <?php
 
+//* Connnection à la base de donnée
+include_once $_SERVER["DOCUMENT_ROOT"]."/include/bdd/bddConnectByRoot.php";
+//* Get Json of the app
+include_once $_SERVER["DOCUMENT_ROOT"]."/include/json.php";
+
 //* PENALTY => number=$dossard & penalty=$penalty_amount
 //* REVERSE PENALTY => number=$dossard & reverse=1
 
@@ -12,15 +17,6 @@ $penalty_id = isset($_GET['penalty_id']) ? $_GET['penalty_id'] : 'empty';
 $surrend = isset($_GET['surrend']) ? $_GET['surrend'] : 'empty';
 $gate_number = isset($_GET['gate']) ? $_GET['gate'] : 'empty';
 $user_id = 0;
-
-//* VAR DUMP VARIABLE
-echo "competitor_number " .$competitor_number. "<br/>";
-echo "penalty_amount " .$penalty_amount. "<br/>";
-echo "reverse " .$reverse. "<br/>";
-echo "penalty_id " .$penalty_id. "<br/>";
-echo "surrend " .$surrend. "<br/>";
-echo "gate_number " .$gate_number. "<br/>";
-echo "user_id " .$user_id. "<br/>";
 
 if (isset($_GET['number'])) {
     //* Si le $number du competiteur est renseigner
@@ -40,7 +36,7 @@ if (isset($_GET['number'])) {
                 //* Ajout de la pénalité dans tbl 'penalty'
                 $infos['[✔] sendingRequestPenaltyCompetitor'] = "La requête d'ajout de pénalitées du n° '$competitor_number' a été recu par le serveur!";
 
-                try {//INSERT INTO race1 (number, StartTime) VALUES (?,?)
+                try {
                     $sqlJudgeCompetitor = "INSERT INTO penalty (gate_number, competitor_number, penalty_amount, user_id) VALUES (?,?,?,?)";
                     $qJudgeCompetitor = $bdd->prepare($sqlJudgeCompetitor);
                     $qJudgeCompetitor->execute([$gate_number, $competitor_number, $penalty_amount, $user_id]);
@@ -49,28 +45,12 @@ if (isset($_GET['number'])) {
                     $warning['errorRequestJudgeCompetitor'] = $e->GETMessage();
                 }
 
-                //* Calcul de penalty dans tbl 'race1'
-                //* 1) Selection des penalité pour le competiteur x
-                try {
-                    $sqlSelectPenalty = "SELECT race1.number, penalty.penalty_amount FROM race1, penalty WHERE race1.number = penalty.competitor_number && race1.number = $competitor_number";
-                    $qSelectPenalty = $bdd->prepare($sqlSelectPenalty);
-                    $qSelectPenalty->execute();
-                } catch (PDOException $e) {
-                    $warning['errorRequestSelectPenalty'] = $e->GETMessage();
-                }
-                
-                $penalty_total = NULL;
-            
-                foreach ($qSelectPenalty as $dataSelectPenalty) {
-                    $penalty_total += $dataSelectPenalty['penalty_amount'];
-                }
-
-                    echo "<br/><br/> Penalité total: $penalty_total <br/><br/>";
+                SumPenalty($competitor_number);
 
             }elseif ($surrend == 1) {
                 
                 $infos['[✔] sendingRequestSurrendCompetitor'] = "La requête d'abandon du n° '$competitor_number' a été recu par le serveur!";
-
+                SumPenalty($competitor_number);
             }else {
                 # code...
             }
@@ -79,7 +59,7 @@ if (isset($_GET['number'])) {
 
                     $infos['[✔] sendingRequestReversePenaltyCompetitor'] = "La requête de suppression de pénalitées du n° '$competitor_number' a été recu par le serveur!";
 
-                    try {//INSERT INTO race1 (number, StartTime) VALUES (?,?)
+                    try {
                         $sqlReverseJudgingCompetitor = "DELETE FROM penalty WHERE id = $penalty_id";
                         $qReverseJudgingCompetitor = $bdd->prepare($sqlReverseJudgingCompetitor);
                         $qReverseJudgingCompetitor->execute();
@@ -98,4 +78,49 @@ if (isset($_GET['number'])) {
     $warning["[✖] incorrectNumberOfSelectedCompetitor"] = "Le n° de dossard du compétiteur sélectionné '$competitor_number' est non conforme! [\$number > 0]";
 }  
 
+}
+
+/**
+ * Make the sum of all penalty of a specified competitor
+ *
+ * @param int competitor_id
+ * 
+ * @author LOUVIGNY Raphaël <https://github.com/TheFrenchCoder>
+ * @return true when succes
+ */ 
+function SumPenalty(int $competitor_number)
+{
+    global $bdd, $json;
+    $penalty_total = NULL;
+    $CurrentRace = "race1";
+
+    // Get penalties of the competitor
+    try {
+        $sqlSelectPenalty = "SELECT SUM(penalty_amount) AS penalty_amount FROM $CurrentRace, penalty WHERE ".$CurrentRace.".number = penalty.competitor_number && ".$CurrentRace.".number = :competitor_number";
+        $qSelectPenalty = $bdd->prepare($sqlSelectPenalty);
+        $qSelectPenalty->execute([$competitor_number]);
+        $infos['[✔] selectPenalty'] = "Les pénalitées de $competitor_number ont toutes été récupérées";
+    } catch (PDOException $e) {
+        $warning['errorRequestSelectPenalty'] = $e->GETMessage();
+    }
+
+    // Calcula of the sum of penalties make directly in the sql request so:
+    foreach ($qSelectPenalty as $a) {
+        var_dump($a);
+        $penalty_total = $a['penalty_amount'];
+    }
+    $infos['[✔] AdditionOfAllPenalty'] = "Les pénalitées de $competitor_number sont $penalty_total";
+
+    // Set the new sum of penalties into the current race table
+    try {
+        $sqlUpdateSumPenalty = "UPDATE $CurrentRace SET penalty = $penalty_total";
+        $qUpdateSumPenalty = $bdd->prepare($sqlUpdateSumPenalty);
+        $qUpdateSumPenalty->execute();
+
+        $infos['[✔] sendingRequestSurrendCompetitor'] = "Les pénalitées de $competitor_number ont toutes été actualisées pour les résultats";
+    } catch (PDOException $e) {
+        $warning['errorRequestUpdateSumPenalty '] = $e->GETMessage();
+    }
+
+    return true;
 }
