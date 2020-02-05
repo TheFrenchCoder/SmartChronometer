88a97930ed3f4a58f7ebd6931c901413987da872
TheFrenchCoder <raphaelouvigny@gmail.com>
2020-02-05 20:56:21 +0400

    Reset app in order to use Symphony - 3
    Reset all, I made a mistake when I merged it with
    "composer create-project symfony/website-skeleton"

--- debug/shell/reset.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE race1 'AUTO_INCREMENT' = '1'
-TRUNCATE `dbchrono`.`race1`
-
-ALTER TABLE penalty 'AUTO_INCREMENT' = '1'
-TRUNCATE `dbchrono`.`penalty`
-
-UPDATE `competitors` SET `IsOnStart`=1,`IsOnRun`=0,`IsFinish`=0,`IsHere`=1
